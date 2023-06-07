03327bb71e20188f24447134a7b466f3cb303b7c
Zak Franczak <zakfranczak@gmail.com>
2023-06-07 16:21:04 -0500

    made WindowQuestion wait to render until cityClick

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { useState } from 'react';
 import Header from './components/Header';
 import Weather from './components/Weather';
 import CurrentWeather from './components/CurrentWeather';
-import './App.css';
 import WindowQuestion from './components/WindowQuestion';
+import './App.css';
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
+  const [cityClicked, setCityClicked] = useState(false);
+
   const handleWeatherData = (data) => {
     setWeatherData(data);
+    setCityClicked(true);
   };
 
   return (
     <div className='background'>
       <div className='container'>
         <Header />
-        <WindowQuestion weatherData={weatherData} />
+        {cityClicked && <WindowQuestion weatherData={weatherData} />}{' '}
         <div className='weather-container'>
           <CurrentWeather weatherData={weatherData} />
           <Weather setWeatherData={handleWeatherData} />
