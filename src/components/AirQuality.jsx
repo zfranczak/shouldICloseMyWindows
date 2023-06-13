6c10d8ac27fb30d6084af1025b0a453f5d0c0c52
Zak Franczak <zakfranczak@gmail.com>
2023-06-13 16:18:09 -0500

    fine tuning AQ settings and added styling for AQ

--- src/components/AirQuality.jsx
+++ src/components/AirQuality.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import '../styles/aq.scss';
 
 const AirQuality = ({ lat, lon }) => {
   const apiKey = import.meta.env.VITE_OPEN_AQ_API_KEY;
   const [pmValue, setPmValue] = useState(null);
+  const [error, setError] = useState(false);
+  const [unit, setUnit] = useState(null);
 
   const roundedLat = lat.toFixed(2);
   const roundedLon = lon.toFixed(2);
@@ -17,11 +20,18 @@ const AirQuality = ({ lat, lon }) => {
 
         if (data) {
           console.log(data); // Log the data to the console
-          const pmValue = data.results[0].measurements[2].value;
-          setPmValue(pmValue);
+          if (data.results.length > 0) {
+            const pmValue = data.results[0].measurements[5].value;
+            const unit = data.results[0].measurements[5].unit;
+            setPmValue(pmValue);
+            setUnit(unit);
+          } else {
+            setError(true);
+          }
         }
       } catch (error) {
         console.log('Error:', error);
+        setError(true);
       }
     };
 
@@ -30,9 +40,15 @@ const AirQuality = ({ lat, lon }) => {
 
   return (
     <div>
-      <h1>Latitude: {roundedLat}</h1>
-      <h1>Longitude: {roundedLon}</h1>
-      <h1>Air Quality: {pmValue}</h1>
+      <h1 className='aq-font'>Latitude: {roundedLat}</h1>
+      <h1 className='aq-font'>Longitude: {roundedLon}</h1>
+      {error ? (
+        <h1 className='aq-font'>No air quality data available</h1>
+      ) : (
+        <h1 className='aq-font'>
+          Air Quality: {pmValue} {unit}
+        </h1>
+      )}
     </div>
   );
 };
